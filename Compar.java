56c2a7f0b0080030f48843944bd02f964eba5ee0
Hema-Gantasala <80663245+Hema-Gantasala@users.noreply.github.com>
2021-07-02 22:37:24 +0530

    Create Compar.java

---
+++ Compar.java
@@ -0,0 +1,11 @@
+class Compar implements Comparator
+{
+public int compare(Object o1, Object o2)
+{
+   Employee e1=(Employee)o1;
+   Employee e2=(Employee)o2;
+   String s1= e1.name;
+   String s2=e2.name;
+   return s1.compareTo(s2);
+}
+}
