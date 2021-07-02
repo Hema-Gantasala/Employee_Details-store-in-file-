5dbae66d8b21f7accb3afadd6a1f4d8d23ca5a92
Hema-Gantasala <80663245+Hema-Gantasala@users.noreply.github.com>
2021-07-02 22:33:37 +0530

    Create Manager.java

---
+++ Manager.java
@@ -0,0 +1,13 @@
+ final class Manager extends Employee
+{
+      
+      Manager()
+       {
+       sal=50000;
+        Designation="Manager";
+      }
+    public void raiseSal()
+     {
+        sal=sal+(sal*10)/100;
+      }
+  } 
